95de6f86c9b174f1e8c7ea0982aeb17523e24b81
DougCornelius80 <DougCornelius80@gmail.com>
2024-11-01 19:19:25 -0500

    logger

--- src/LoggingExample.java
+++ src/LoggingExample.java
@@ -1,2 +1,17 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class LoggingExample {
+
+    // Instantiate Logging Object to be used by the class
+    final static Logger log =
+            LogManager.getLogger(LoggingExample.class.getName());
+
+    public static void main(String[] args) {
+
+        log.debug("Hello World!");
+        log.info("Hello World, Again!");
+
+    }
+
 }
