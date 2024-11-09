08c363f762ebe52a2175b3bb0fcf0a595fc6c5b1
NWTC-KP <peotterkevin@gmail.com>
2024-11-08 19:09:31 -0600

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
+//Logging Stuff
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.Logger;
+
 public class Main {
+    //Logger object
+    final static Logger log = LogManager.getLogger(Main.class.getName());
+
     public static void main(String[] args)
     {
-        //Team Stuff
-
         //Kevin's edit
-        //Nothing of value but comment
+        //Nothing of value but comments
         //Here to demonstrate a push
+        try {
+            System.out.println("Hello world!!!!!");
+            log.debug("Hello World!");
+        }
+        catch (Exception ex) {
+            System.out.println("An Exception Has Occured...");
+        }
+        finally {
+            //Clear scanners and other things
+        }
     }
 }
