d7ee1ab59d8628c8e4b20ce479dad77301026133
Mindlock7 <mindlock7@gmail.com>
2024-11-22 17:54:59 -0600

    More fixes and additions (mainly to key class)

--- src/Key.java
+++ src/Key.java
@@ -1,36 +1,43 @@
 public class Key {
     //General Item Properties
-    Boolean MultipleUses = false; //Incase you want this to break multiple walls or open many locks
+    private Boolean MultipleUses = false; //Incase you want this to break multiple walls or open many locks
 
     //Unlocking ID for making sure it only unlocks one kind of lock
-    Integer UnlockId = 0;
+    private Integer UnlockId;
     //When it can unlock/break something
-    String UnlockText = "You put the key in the lock and twisted as it clicks open";
+    private String UnlockText = "You put the key in the lock and twist as it clicks open";
     //When it's unlockable but not the right ID
-    String NoUnlockText = "You put the key into the lock but it doesn't budge, seems like this is the wrong key";
+    private String NoUnlockText = "You put the key into the lock but it doesn't budge, seems like this is the wrong key";
     //When it's not unlockable at all
-    String NotUnlockableText = "There doesn't appear to be a lock";
+    private String NotUnlockableText = "There doesn't appear to be a lock";
 
-    //Just UnlockID key creation
-    public Key(Integer unlockId) {
+    //Desciption of the item
+    private String Description;
+
+    //UnlockID and description key creation
+    public Key(Integer unlockId, String description) {
         UnlockId = unlockId;
+        Description = description;
     }
 
-    public Key(Integer unlockId, Boolean multipleUses) {
+    public Key(Integer unlockId, String description, Boolean multipleUses) {
         UnlockId = unlockId;
+        Description = description;
         MultipleUses = multipleUses;
     }
 
     //Everything key creation
-    public Key(Integer unlockId, String unlockText, String noUnlockText, String notUnlockableText) {
+    public Key(Integer unlockId, String description, String unlockText, String noUnlockText, String notUnlockableText) {
         UnlockId = unlockId;
+        Description = description;
         UnlockText = unlockText;
         NoUnlockText = noUnlockText;
         NotUnlockableText = notUnlockableText;
     }
 
-    public Key(Integer unlockId, String unlockText, String noUnlockText, String notUnlockableText, Boolean multipleUses) {
+    public Key(Integer unlockId, String description, String unlockText, String noUnlockText, String notUnlockableText, Boolean multipleUses) {
         UnlockId = unlockId;
+        Description = description;
         UnlockText = unlockText;
         NoUnlockText = noUnlockText;
         NotUnlockableText = notUnlockableText;
@@ -39,8 +46,14 @@ public class Key {
 
 
     //Methods
-    public boolean Use() {
+    public void Check() {
+        System.out.println(Description);
+    }
 
+    public boolean Use() {
+        //logic here (for checking if the thing the player is looking or using the key on can be unlocked, and whether
+        //it matches the rest of the UnlockID
+        System.out.println(UnlockText);
         return true;
     }
 }
